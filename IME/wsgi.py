8b44d45c66f6bc4e62c51ffd3608846e74aae4db
Mohamad Latifian <mohamad.latifian@gmail.com>
2016-01-12 21:05:11 +0330

    heroku

--- IME/wsgi.py
+++ IME/wsgi.py
@@ -1,6 +1,8 @@
 
+import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IME.settings")
 application = DjangoWhiteNoise(application)
