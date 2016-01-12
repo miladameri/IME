17b2c8c4b1a469b8bddd4c59744c428d43701abf
Mohamad Latifian <mohamad.latifian@gmail.com>
2016-01-12 21:16:53 +0330

    size change for heroku

--- IME/wsgi.py
+++ IME/wsgi.py
@@ -3,6 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IME.settings")
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
