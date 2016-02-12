733069059549951f4f2434680358f2770bb4b115
Mohamad Latifian <mohamad.latifian@gmail.com>
2016-02-12 16:41:58 +0330

    data base configurations

--- imev0/admin.py
+++ imev0/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from imev0.models import Transaction
 
-admin.site.register(Transaction)
+class TAdmin(admin.ModelAdmin):
+    list_display = ('date', 'product', 'producer', 'supply')
+
+
+admin.site.register(Transaction, TAdmin)
